bf13b700214ed1edd60afdcaa6d186aca4e5a119
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-27 14:58:29 +0900

    work

--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class SimpleCalculatorTest {
     @Test //테스트케이스 1개 만듦
     @DisplayName("1+2=3")
-    public void testPlus(){
+    public void t1(){
         SimpleCalculator simpleCalculator=new SimpleCalculator();
         int rs=simpleCalculator.plus(1,2);
 
         Assertions.assertEquals(3,rs);
+    }
+
+    @Test //테스트케이스 2개 만듦
+    @DisplayName("2+10=12")
+    public void t2(){
+        SimpleCalculator simpleCalculator=new SimpleCalculator();
+        int rs=simpleCalculator.plus(2,10);
+
+        Assertions.assertEquals(12,rs);
     }
 }
